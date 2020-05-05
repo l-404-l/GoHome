5f654f4f7fecafee408c1684dbafe5bcc1ab3026
l-404-l <saywhattowhat@gmail.com>
2020-05-05 18:55:38 -0500

    Add project files.

---
+++ GoHome/Main.cs
@@ -0,0 +1,38 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+using VRC.Core;
+
+namespace GoHome
+{
+    public static class BuildInfo
+    {
+        public const string Name = "Clean Console"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "404#0004"; // Author of the Mod.  (Set as null if none)
+        public const string Company = "I am not a company -Kappa-"; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = "https://github.com/l-404-l"; // Download Link for the Mod.  (Set as null if none)
+    }
+    public class Main : MelonMod
+    {
+        public override void VRChat_OnUiManagerInit()
+        {
+            var savelist = new List<string>();
+            
+            var buttont = QuickMenu.prop_QuickMenu_0.transform.Find("ShortcutMenu/GoHomeButton");
+            var button = buttont.GetComponentInChildren<UnityEngine.UI.Button>();
+            var deflt = button.onClick;
+            button.onClick = new UnityEngine.UI.Button.ButtonClickedEvent();
+            button.onClick.AddListener(new Action(() => {
+                if (Input.GetKey(KeyCode.LeftShift))
+                    VRCFlowManager.field_Private_Static_VRCFlowManager_0.Method_Public_Virtual_New_Void_2();
+                else
+                    deflt.Invoke();
+            }));
+        }
+    }
+}
---
+++ GoHome/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(GoHome.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(GoHome.BuildInfo.Company)]
+[assembly: AssemblyProduct(GoHome.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + GoHome.BuildInfo.Author)]
+[assembly: AssemblyTrademark(GoHome.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(GoHome.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(GoHome.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonModInfo(typeof(GoHome.Main), GoHome.BuildInfo.Name, GoHome.BuildInfo.Version, GoHome.BuildInfo.Author, GoHome.BuildInfo.DownloadLink)]
+[assembly: MelonModGame("VRChat", "VRChat")]
